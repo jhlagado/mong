d2baa4796db42b339c09aecb7bd2bbd85d0ec449
John Hardy <jh@lagado.com>
2019-06-14 13:39:26 +0000

    fixes

--- 3-insert.js
+++ 3-insert.js
@@ -5,7 +5,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
+  dbo.collection("customers").insertOne(myobj, function(err) {
     if (err) throw err;
     console.log("1 document inserted");
     db.close();
