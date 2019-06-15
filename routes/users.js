8d85d349810df09817c3faa16755cfde0ee0cc4c
hurricanew <hurricanew@hotmail.com>
2019-06-15 13:27:32 +0000

    menu highlight, login, register 6

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.route('/login')
+  .get((req, res) => {
+    res.render('login', {title: 'Login'})
+  })
+router.route("/register")
+  .get((req, res) => {
+    res.render('register', { title: 'Register' });
+  })
 module.exports = router;
