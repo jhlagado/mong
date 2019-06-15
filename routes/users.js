5a977d306dc2f5660e893311ef809f97a1ed8841
hurricanew <hurricanew@hotmail.com>
2019-06-15 13:57:41 +0000

    form field validation 8

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,39 @@ router.route("/register")
     res.render('register', { title: 'Register' });
   })
   .post(upload.single('profileImage'),(req, res) => {
-    console.log(req.body);
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    console.log(req.file);
+
+    //if there is a file
+    if (req.file) {
+      console.log('Uploading File ...');
+      var profileImage = req.file.filename;
+      console.log(profileImage);
+    } else {
+      console.log('No File Uploaded ...');
+      var profileImage = 'noimage.jpg';
+    }
+
+    console.log(profileImage);
+    // Form validator
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Confirm-password field is required').notEmpty();
+    req.checkBody('password2', 'Password not match').equals(req.body.password);
+
+    // Check errors
+    let errors = req.validationErrors();
+    if (errors) {
+      res.render('register', {errors: errors});
+    } else {
+        console.log('no erros');
+    }
   })
 module.exports = router;
