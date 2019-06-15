6fb4be7fbf6e29ee6bc535f56c296c7bf8b96353
hurricanew <hurricanew@hotmail.com>
2019-06-15 13:39:28 +0000

    register, upload image 7

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+//file upload
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,5 +16,8 @@ router.route('/login')
 router.route("/register")
   .get((req, res) => {
     res.render('register', { title: 'Register' });
+  })
+  .post(upload.single('profileImage'),(req, res) => {
+    console.log(req.body);
   })
 module.exports = router;
