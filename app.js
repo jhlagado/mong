4d8da0ea5dafb0c952fc8f04309ab1c7ec11d385
John Hardy <jh@lagado.com>
2019-09-24 10:27:16 +0000

    starting point 1

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 
 app.use(function(req, res, next) {
