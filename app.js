05e7359142e55f6332fb5bb3503f5077b347b4f3
John Hardy <jh@lagado.com>
2019-09-24 10:21:20 +0000

    starting point -2

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const path = require('path');
 
 const indexRouter = require('./routes/index');
-const customerRoutes = require('./routes/customer');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/customers', customerRoutes);
 
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/customer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/customers', function(req, res, next) {
- res.send('respond with a resource');
-});
-
-module.exports = router;
