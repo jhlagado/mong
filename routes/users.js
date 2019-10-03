d5edcddb1eede2ed592841611faacb47e5b4856f
John Hardy <jh@lagado.com>
2019-10-03 10:45:27 +0000

    - logout

--- routes/users.js
+++ routes/users.js
@@ -80,4 +80,10 @@ router.route('/login')
     res.redirect('/');
   });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out');
+  res.redirect('/users/login');
+});
+
 module.exports = router;
